2c3e1fc6e6b2cb84f8395923d53a2790a2897619
Jonathan Clem <j@jclem.me>
2022-05-26 15:15:42 -0400

    Clean up command/label aggregation

--- konk/cmd/p.go
+++ konk/cmd/p.go
@@ -2,10 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/jclem/konk/konk"
-	"github.com/mattn/go-shellwords"
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
 )
@@ -15,44 +13,17 @@ var pnames []string
 var PCommand = cobra.Command{
 	Use:   "p <command...>",
 	Short: "Run commands in parallel",
-	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		commands := make([][]string, len(args))
-		labels := make([]string, len(args))
+		commandStrings, commands, err := collectCommands(args)
+		if err != nil {
+			return err
+		}
 
-		if len(pnames) > 0 && len(pnames) != len(args) {
+		if len(pnames) > 0 && len(pnames) != len(commands) {
 			return fmt.Errorf("number of names must match number of commands")
 		}
 
-		for i, cmd := range args {
-			args, err := shellwords.Parse(cmd)
-
-			if err != nil {
-				return err
-			}
-
-			commands[i] = args
-
-			if cmdAsLabel {
-				labels[i] = cmd
-			} else if len(pnames) > 0 {
-				labels[i] = pnames[i]
-			} else {
-				labels[i] = fmt.Sprintf("%d", i)
-			}
-		}
-
-		var maxLabelLen int
-
-		for _, label := range labels {
-			if len(label) > maxLabelLen {
-				maxLabelLen = len(label)
-			}
-		}
-
-		for i, label := range labels {
-			labels[i] = fmt.Sprintf("%s%s", label, strings.Repeat(" ", maxLabelLen-len(label)))
-		}
+		labels := collectLabels(commandStrings)
 
 		var eg errgroup.Group
 
--- konk/cmd/s.go
+++ konk/cmd/s.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/jclem/konk/konk"
-	"github.com/mattn/go-shellwords"
 	"github.com/spf13/cobra"
 )
 
@@ -13,32 +12,17 @@ var snames []string
 var SCommand = cobra.Command{
 	Use:   "s <command...>",
 	Short: "Run commands in serial",
-	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		commands := make([][]string, len(args))
-		labels := make([]string, len(args))
+		commandStrings, commands, err := collectCommands(args)
+		if err != nil {
+			return err
+		}
 
-		if len(snames) > 0 && len(snames) != len(args) {
+		if len(pnames) > 0 && len(pnames) != len(commands) {
 			return fmt.Errorf("number of names must match number of commands")
 		}
 
-		for i, cmd := range args {
-			args, err := shellwords.Parse(cmd)
-
-			if err != nil {
-				return err
-			}
-
-			commands[i] = args
-
-			if cmdAsLabel {
-				labels[i] = cmd
-			} else if len(snames) > 0 {
-				labels[i] = snames[i]
-			} else {
-				labels[i] = fmt.Sprintf("%d", i)
-			}
-		}
+		labels := collectLabels(commandStrings)
 
 		for i, cmd := range commands {
 			c := konk.NewCommand(konk.CommandConfig{
