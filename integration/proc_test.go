40ed477773ac3c2e914ac380e4ba2e5f27b30e0b
Jonathan Clem <j@jclem.me>
2022-09-20 16:49:15 -0400

    Add more env tests to proc tests

--- integration/proc_test.go
+++ integration/proc_test.go
@@ -26,5 +26,52 @@ func TestProc(t *testing.T) {
 
 	assert.Equal(t, `
 [echo-a] a
-[echo-b] b`, sortedOut, "output did not match expected output")
+[echo-b] b
+[echo-c] `, sortedOut, "output did not match expected output")
+}
+
+func TestProcWithExternalEnvNoEnv(t *testing.T) {
+	out := new(strings.Builder)
+	cmd := exec.Command(
+		"bin/konk", "proc", "-E",
+		"-w", "fixtures/proc")
+	cmd.Env = append(cmd.Env, "A=new-a")
+	cmd.Env = append(cmd.Env, "B=new-b")
+	cmd.Env = append(cmd.Env, "C=new-c")
+	cmd.Stdout = out
+	cmd.Stderr = out
+	if err := cmd.Run(); err != nil {
+		t.Error(err)
+	}
+
+	lines := strings.Split(out.String(), "\n")
+	sort.Strings(lines)
+	sortedOut := strings.Join(lines, "\n")
+
+	assert.Equal(t, `
+[echo-a] new-a
+[echo-b] new-b
+[echo-c] new-c`, sortedOut, "output did not match expected output")
+}
+
+func TestProcWithExternalEnvAndEnv(t *testing.T) {
+	out := new(strings.Builder)
+	cmd := exec.Command(
+		"bin/konk", "proc",
+		"-w", "fixtures/proc")
+	cmd.Env = append(cmd.Env, "C=c")
+	cmd.Stdout = out
+	cmd.Stderr = out
+	if err := cmd.Run(); err != nil {
+		t.Error(err)
+	}
+
+	lines := strings.Split(out.String(), "\n")
+	sort.Strings(lines)
+	sortedOut := strings.Join(lines, "\n")
+
+	assert.Equal(t, `
+[echo-a] a
+[echo-b] b
+[echo-c] c`, sortedOut, "output did not match expected output")
 }
