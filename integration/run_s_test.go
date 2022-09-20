84f9723c0aca33b8b380158b59da7c39a51e4871
Jonathan Clem <j@jclem.me>
2022-09-20 14:18:37 -0400

    Add more run_s tests

--- integration/run_s_test.go
+++ integration/run_s_test.go
@@ -12,6 +12,7 @@ func TestRunSerially(t *testing.T) {
 	out := new(strings.Builder)
 	cmd := exec.Command("go", "run", "..", "run", "serially", "echo a", "echo b", "echo c")
 	cmd.Stdout = out
+	cmd.Stderr = out
 	if err := cmd.Run(); err != nil {
 		t.Error(err)
 	}
@@ -28,11 +29,91 @@ func TestRunSeriallyWithLabels(t *testing.T) {
 		"-l", "a", "-l", "b", "-l", "c",
 		"echo a", "echo b", "echo c")
 	cmd.Stdout = out
+	cmd.Stderr = out
 	if err := cmd.Run(); err != nil {
 		t.Error(err)
 	}
 	assert.Equal(t, `[a] a
 [b] b
 [c] c
+`, out.String(), "output did not match expected output")
+}
+
+func TestRunSeriallyWithLabelsMismatch(t *testing.T) {
+	out := new(strings.Builder)
+	cmd := exec.Command(
+		"go", "run", "..", "run", "serially",
+		"-l", "a", "-l", "b",
+		"echo a", "echo b", "echo c")
+	cmd.Stdout = out
+	cmd.Stderr = out
+
+	err := cmd.Run()
+	if assert.Error(t, err) {
+		assert.IsType(t, &exec.ExitError{}, err)
+	}
+
+	assert.Equal(t, "Error: number of names must match number of commands\nexit status 1\n", out.String(), "error output did not match expectation")
+}
+
+func TestRunSeriallyWithCommandLabels(t *testing.T) {
+	out := new(strings.Builder)
+	cmd := exec.Command(
+		"go", "run", "..", "run", "serially", "-L",
+		"echo a", "echo b", "echo c")
+	cmd.Stdout = out
+	if err := cmd.Run(); err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, `[echo a] a
+[echo b] b
+[echo c] c
+`, out.String(), "output did not match expected output")
+}
+
+func TestRunSeriallyWithNpm(t *testing.T) {
+	out := new(strings.Builder)
+	cmd := exec.Command(
+		"go", "run", "..", "run", "serially",
+		"-w", "fixtures/npm",
+		"--npm", "echo-a",
+		"--npm", "echo-b")
+	cmd.Stdout = out
+	if err := cmd.Run(); err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, `[0] 
+[0] > echo-a
+[0] > echo a
+[0] 
+[0] a
+[1] 
+[1] > echo-b
+[1] > echo b
+[1] 
+[1] b
+`, out.String(), "output did not match expected output")
+}
+
+func TestRunSeriallyWithNpmGlob(t *testing.T) {
+	out := new(strings.Builder)
+	cmd := exec.Command(
+		"go", "run", "..", "run", "serially",
+		"-w", "fixtures/npm",
+		"--npm", "echo-*")
+	cmd.Stdout = out
+	if err := cmd.Run(); err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, `[0] 
+[0] > echo-a
+[0] > echo a
+[0] 
+[0] a
+[1] 
+[1] > echo-b
+[1] > echo b
+[1] 
+[1] b
 `, out.String(), "output did not match expected output")
 }
