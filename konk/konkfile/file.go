382e23c3ba5a6841bd5bb32a5e31416e8f6da5c6
Jonathan Clem <jclem@magic.dev>
2024-03-20 12:44:44 -0400

    Fix lint errors

--- konk/konkfile/file.go
+++ konk/konkfile/file.go
@@ -1,11 +1,11 @@
 package konkfile
 
 type File struct {
-	Commands map[string]Command `json:"commands" yaml:"commands"`
+	Commands map[string]Command `json:"commands" toml:"commands" yaml:"commands"`
 }
 
 type Command struct {
-	Run       string   `json:"run"       yaml:"run"`
-	Needs     []string `json:"needs"     yaml:"needs"`
-	Exclusive bool     `json:"exclusive" yaml:"exclusive"`
+	Run       string   `json:"run"       toml:"run"       yaml:"run"`
+	Needs     []string `json:"needs"     toml:"needs"     yaml:"needs"`
+	Exclusive bool     `json:"exclusive" toml:"exclusive" yaml:"exclusive"`
 }
